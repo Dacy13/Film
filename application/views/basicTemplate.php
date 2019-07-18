8b44748c0b09a801b03ac8f2905da8aeb51bda30
Dacy13 <51496408+Dacy13@users.noreply.github.com>
2019-07-18 21:47:30 +0200

    Update basicTemplate.php

--- application/views/basicTemplate.php
+++ application/views/basicTemplate.php
@@ -1,16 +1,12 @@
 <?php
 
 
-//    if ($this->session->user == null ) {
-//     
-//        $this->load->view( "header/guestHeader" );    
-//    }
-//    else{
-//        $this->load->view( "header/userHeader" );
-//            }
-//        $this->load->view( $middle, $middleData ?? [] );
-//        $this->load->view( "footer/basicFooter" );
-       
-   $this->load->view ( "header/userHeader");
-   $this->load->view ( $middle, $middleData ?? [] );
-   $this->load->view ( "footer/basicFooter");
+    if (!$this->session->has_userdata('korisnik')) {
+     
+        $this->load->view( "header/guestHeader", $headerData ?? [] );    
+    }
+    else{
+        $this->load->view( "header/userHeader", $headerData ?? [] );
+            }
+        $this->load->view( $middle, $middleData ?? [] );
+        $this->load->view( "footer/basicFooter" );
