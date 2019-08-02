bfde0036670452f882a9f1b59709c62cb2ee4ba4
Dacy13 <51496408+Dacy13@users.noreply.github.com>
2019-08-02 20:03:17 +0200

    Update mojNalog.php

--- application/views/middle/mojNalog.php
+++ application/views/middle/mojNalog.php
@@ -1,65 +1,72 @@
-<div class="d-flex justify-content-center ">
-    <div class="container-fluid justify-content-center">
+<div class="text-center">
+      <h3>
+           Podaci o korisniku:
+     </h3>
+</div>
+<!--<div class="col-md-4 col-md-offset-4">my div here</div>-->
+<div class="container">
+    <div class="align-items-center justify-content-center">
 <form name="promeniPodatke" method="POST" action="<?php echo site_url('KorisnikKontroler/izmena');?>">
         <?php foreach($podaci as $p) { ?>   
         <div class="form-group row">
-            <label for="inputUsername" class="col-sm-2 col-form-label">Username</label>
+            <label for="inputUsername" class="col-sm-2 col-form-label">Username: </label>
                 <div class="col-sm-10">
-                    <input type="email" class="col-sm-2 col-form-label" name="username" disabled value="<?php echo $p->Username ?? "" ?>">
+                    <input type="email" class="col-sm-2 form-control" name="username" disabled value="<?php echo $p->Username ?? "" ?>">
                 </div>
         </div>
         <div class="form-group row">
-            <label for="inputName" class="col-sm-2 col-form-label">Ime</label>
+            <label for="inputName" class="col-sm-2 col-form-label">Ime: </label>
                 <div class="col-sm-10">
-                    <input type="text" class="col-sm-2 col-form-label" name="ime" value="<?php echo $p->Name ?? "" ?>" >
+                    <input type="text" class="col-sm-2 form-control" name="ime" value="<?php echo $p->Name ?? "" ?>" >
                 </div>
         </div>
         <div class="form-group row">
-            <label for="inputSurname" class="col-sm-2 col-form-label">Prezime</label>
+            <label for="inputSurname" class="col-sm-2 col-form-label">Prezime: </label>
                 <div class="col-sm-10">
-                    <input type="text" class="col-sm-2 col-form-label" name="prezime" value="<?php echo $p->Surname ?? ""; ?>">
+                    <input type="text" class="col-sm-2 form-control" name="prezime" value="<?php echo $p->Surname ?? ""; ?>">
                 </div>
         </div>
         <div class="form-group row">
-            <label for="inputDate" class="col-sm-2 col-form-label">Godina rodjenja</label>
+            <label for="inputDate" class="col-sm-2 col-form-label">Godina rodjenja: </label>
                 <div class="col-sm-10">
-                    <input type="text" class="col-sm-2 col-form-label" name="godina" disabled value="<?php echo $p->DateOfBirth ?? ""; ?>">
+                    <input type="text" class="col-sm-2 form-control" name="godina" disabled value="<?php echo $p->DateOfBirth ?? ""; ?>">
                 </div>
         </div>
         <div class="form-group row">
-            <label for="inputBroj" class="col-sm-2 col-form-label">Broj telefona</label>
+            <label for="inputBroj" class="col-sm-2 col-form-label">Broj telefona: </label>
                 <div class="col-sm-10">
-                    <input type="text" class="col-sm-2 col-form-label" name="broj" value="<?php echo $p->Mobile ?? ""; ?>">
+                    <input type="text" class="col-sm-2 form-control" name="broj" value="<?php echo $p->Mobile ?? ""; ?>">
                 </div>
         </div>
         <div class="form-group row">
-            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+            <label for="inputEmail" class="col-sm-2 col-form-label">E-mail:</label>
                 <div class="col-sm-10">
-                    <input type="email" class="col-sm-2 col-form-label" name="mejl" value="<?php echo $p->Email ?? ""; ?>">
+                    <input type="email" class="col-sm-2 form-control" name="mejl" value="<?php echo $p->Email ?? ""; ?>">
                 </div>
         </div>
         <label>Promenite lozinku</label>
         <div class="form-group row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Trenutna lozinka</label>
+            <label for="inputPassword" class="col-sm-2 col-form-label">Trenutna lozinka: </label>
                 <div class="col-sm-10">
-                    <input type="password" class="col-sm-2 col-form-label" name="password" value="<?php echo $p->Password ?? ""; ?>" >
+                    <input type="password" class="col-sm-2 form-control" name="password" value="<?php echo $p->Password ?? ""; ?>" >
                 </div>
         </div>
         <div class="form-group row">
-            <label for="inputNova" class="col-sm-2 col-form-label">Nova lozinka</label>
+            <label for="inputNova" class="col-sm-2 col-form-label">Nova lozinka: </label>
                 <div class="col-sm-10">
-                    <input type="password" class="col-sm-2 col-form-label" name="novip">
+                    <input type="password" class="col-sm-2 form-control" name="novip">
                 </div>
         </div>
         <div class="form-group row">
-            <label for="inputPotvrda" class="col-sm-2 col-form-label">Potvrdite lozinku</label>
+            <label for="inputPotvrda" class="col-sm-2 col-form-label">Potvrdite lozinku: </label>
                 <div class="col-sm-10">
-                    <input type="password" class="col-sm-2 col-form-label" name="potvrda" >
+                    <input type="password" class="col-sm-2 form-control" name="potvrda" >
                 </div>
         </div>
         <div class="form-row">
+            <label for="#" class="col-sm-2 col-form-label"></label>
             <div class="col-sm-10">
-                <input type="submit" name="izmeni" class="btn btn-primary" value="Izmeni">
+                <input type="submit" name="izmeni" class="btn btn-outline-warning" value="Izmeni podatke">
             </div>
         </div>
         <?php } 
