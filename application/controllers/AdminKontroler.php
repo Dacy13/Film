343ad5a4cdc2c23b574e7742b08a63fd8df4f4d0
Aleksandar <41162669+Aca84@users.noreply.github.com>
2019-08-28 16:28:11 +0200

    Update AdminKontroler.php

--- application/controllers/AdminKontroler.php
+++ application/controllers/AdminKontroler.php
@@ -12,7 +12,6 @@ class AdminKontroler extends CI_Controller {
       $this->load->helper('url');
       $this->load->model("AdminModel");
       $this->load->library('upload');
-
    }
 
    public function index() {
@@ -25,29 +24,9 @@ class AdminKontroler extends CI_Controller {
 //  festivali funkcije /////////////////////////////////
    public function festivaliSvi() {
 
-      $IdFest = $this->input->get('id', TRUE); // OVO JE VAZNO; sa ovim je xxx
-
-      $pocetakF = 0;
-
-      if ($this->uri->segment(3)) {
-
-         $pocetakF = $this->uri->segment(3);
-      }
+      $IdFest    = $this->input->get('id', TRUE); 
       $festivali = $this->AdminModel->sviFestivali();
 
-//      $festivali = $this->AdminModel->sviFestivali($pocetakF, LIMIT_PO_STRANICI);
-
-      $IdFest = $this->input->get('id', TRUE);
-
-//      $this->load->library('pagination');
-//
-//      $config = $this->config->item('pagination');
-//
-//      $config['base_url'] = site_url("AdminKontroler/festivaliSvi");
-//      $config['total_rows'] = $this->AdminModel->prebrojFestivale();
-//      $config['per_page'] = LIMIT_PO_STRANICI;
-//
-//      $this->pagination->initialize($config);
 
       $data = ['festivali' => $festivali];
       $this->load->view('middle/admin/festivaliAdmin', $data);
@@ -55,13 +34,12 @@ class AdminKontroler extends CI_Controller {
 
    public function dohvatiFestival($IdFest) {
 
-      $IdFest = $this->input->get('id', TRUE);
+      $IdFest    = $this->input->get('id', TRUE);
       $festivali = $this->AdminModel->dohvatiFestival($IdFest);
 
       $data = ['festivali' => $festivali];
       $data['middle'] = 'middle/admin/izmeniFest$ival';
-//        $this->load->view("basicTemplate", $data);
-//        $festival=$this->FestivalModel->dohvatiFestival();
+
    }
 
    public function dodajFestival() {
@@ -69,24 +47,15 @@ class AdminKontroler extends CI_Controller {
       $lokacije = $this->AdminModel->sviGradovi();
       $data = ['lokacije' => $lokacije];
       $this->load->view('middle/admin/noviFestival', $data);
-//        $data['middle']='middle/admin/noviFestival';
-//        $this->load->view("basicTemplate", $data);   
 
       if ($this->input->post('dodajFest')) {
 
          $ime_festivala = $this->input->post("ime_festivala");
-         $od = $this->input->post("od");
-         $do = $this->input->post("do");
-         $opis = $this->input->post("opis");
-         $max_Tickets = $this->input->post("max_Tickets");
-         $grad = $this->input->post("gradovi");
-
-//                var_dump($ime_festivala);
-//                var_dump($od);
-//                var_dump($do);
-//                var_dump($opis);
-//                var_dump($max_Tickets);
-//                var_dump($grad);
+         $od            = $this->input->post("od");
+         $do            = $this->input->post("do");
+         $opis          = $this->input->post("opis");
+         $max_Tickets   = $this->input->post("max_Tickets");
+         $grad          = $this->input->post("gradovi");
 
          $this->AdminModel->noviFestival($ime_festivala, $od, $do, $opis, $max_Tickets, $grad);
          redirect('AdminKontroler/index');
@@ -95,36 +64,24 @@ class AdminKontroler extends CI_Controller {
 
    public function izmeniFestival() {
 
-      $IdFest = $this->input->get('id', TRUE); // OVO JE VAZNO; sa ovim je bilo OZBILjNOG (by Dejan Jolovic)
-
+      $IdFest   = $this->input->get('id', TRUE); // OVO JE VAZNO; sa ovim je bilo OZBILjNOG (by Dejan Jolovic)
       $festival = $this->AdminModel->dohvatiFestival($IdFest);
-
-//        $festival=$this->AdminModel->sviFestivali($IdFest);
-//        $projekcije=$this->AdminModel->sveProjekcije($IdFest);
       $lokacije = $this->AdminModel->sviGradovi();
 
       $data = ['festival' => $festival, 'lokacije' => $lokacije];
-//        $data=['lokacije' => $lokacije];  
       $data['middle'] = 'middle/admin/izmeniFestival';
+      
       $this->load->view("basicTemplate", $data);
-//      $this->load->view('middle/admin/izmeniFestival', $data);
 
       if ($this->input->post('izmeniFest')) {
 
-         $IdFest = $this->input->post('hiddenId');
+         $IdFest        = $this->input->post('hiddenId');
          $ime_festivala = $this->input->post("ime_festivala");
-         $od = $this->input->post("od");
-         $do = $this->input->post("do");
-         $opis = $this->input->post("opis");
-         $max_Tickets = $this->input->post("max_Tickets");
-         $grad = $this->input->post("hiddenIdG");
-
-//                var_dump($ime_festivala);
-//                var_dump($od);
-//                var_dump($do);
-//                var_dump($opis);
-//                var_dump($max_Tickets);
-//                var_dump($grad);
+         $od            = $this->input->post("od");
+         $do            = $this->input->post("do");
+         $opis          = $this->input->post("opis");
+         $max_Tickets   = $this->input->post("max_Tickets");
+         $grad          = $this->input->post("hiddenIdG");
 
          $this->AdminModel->izmeniFestivali($IdFest, $ime_festivala, $od, $do, $opis, $max_Tickets, $grad);
          redirect('AdminKontroler/index');
@@ -133,23 +90,26 @@ class AdminKontroler extends CI_Controller {
 
 //  filmovi funkcije //////////////////////////////////////
    public function noviFilm() {
-
+       
+      $film = $this->AdminModel->sviFilmovi();
+      
+      $data = ['film' => $film];
       $data['middle'] = 'middle/admin/noviFilm';
-//        $this->load->view("basicTemplate", $data);          
+         
       $this->load->view('middle/admin/noviFilm', $data);
 
       if ($this->input->post('dodajFilm')) {
 
-         $original = $this->input->post("original");
-         $srpski = $this->input->post("srpski");
-         $rezija = $this->input->post("rezija");
-         $glumci = $this->input->post("glumci");
-         $datum = $this->input->post("datum");
-         $trajanje = $this->input->post("trajanje");
-         $zemlja = $this->input->post("zemlja");
-         $opisF = $this->input->post("opisF");
-         $imdb = $this->input->post("imdb");
-         $poster = $this->input->post("poster");
+         $original  = $this->input->post("original");
+         $srpski    = $this->input->post("srpski");
+         $rezija    = $this->input->post("rezija");
+         $glumci    = $this->input->post("glumci");
+         $datum     = $this->input->post("datum");
+         $trajanje  = $this->input->post("trajanje");
+         $zemlja    = $this->input->post("zemlja");
+         $opisF     = $this->input->post("opisF");
+         $imdb      = $this->input->post("imdb");
+         $poster    = $this->input->post("poster");
 
          $config = $this->config->item('upload');
 
@@ -163,14 +123,13 @@ class AdminKontroler extends CI_Controller {
          if (!$this->upload->do_upload("poster")) {
 
 //                    echo "Sllika nije ubacena!";      
-            $this->form_validation->set_message('poster', 'Niste upload-ovali sliku!');
+//            $this->form_validation->set_message('poster', 'Niste upload-ovali sliku!');
          } else {
             $this->upload->data($poster);
-            echo 'Weeee';
+//            echo 'Weeee';
 //                     $this->form_validation->set_message('poster', 'Niste upload-ovali sliku!');
+//            $this->AdminModel->noviFilm($original, $srpski, $this->upload->data('file_name'), $datum, $opisF, $rezija, $glumci, $trajanje, $zemlja, $imdb); ne radi
          }
-
-         var_dump("poster");
 
          $this->AdminModel->noviFilm($original, $srpski, $this->upload->data('file_name'), $datum, $opisF, $rezija, $glumci, $trajanje, $zemlja, $imdb);
          redirect('AdminKontroler/index');
@@ -264,13 +223,63 @@ class AdminKontroler extends CI_Controller {
       
       $this->AdminModel->otkaziProjekciju($idPro);    
    }  
-	
-//    logout 
-	public function logout(){
+   
+   public function posaljiEmail(){
+         // Load PHPMailer library
+        $this->load->library('phpmailer_lib');
+
+        // PHPMailer object
+        $mail = $this->phpmailer_lib->load();
+
+        // SMTP configuration
+        $mail->isSMTP();
+        $mail->Host     = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'hablanoabla@gmail.com';
+        $mail->Password = 'noablo777hablo';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port     = 465;'********';
+
+        $mail->setFrom('hablanoabla@gmail.com', 'Prodavnica karata za Filmske Festivale');
+//        $mail->addReplyTo('info@example.com', 'CodexWorld');
+
+        // Add a recipient
+        $mail->addAddress('aleksandarvelimirovic7@gmail.com');
+//        $mail->addAddress('.com');
+
+        // Add cc or bcc 
+//        $mail->addCC('cc@example.com');
+//        $mail->addBCC('bcc@example.com');
+
+        // Email subject
+        $mail->Subject = 'Otkazana projekcija';
+
+        // Set email format to HTML
+        $mail->isHTML(true);
+
+        // Email body content
+        $mailContent = 
+//        $this->input->post('poruka');
+            "<h1>Otazana projekcija</h1>
+//           <p>236 red poruka poslata iz kotrolera.</p>";
+        $mail->Body = $mailContent;
+
+        // Send email
+        if(!$mail->send()){
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        }else{
+            echo 'Message has been sent';
+        }
+    }
+    
+//   logout   
+    public function logout(){
         
         $this->session->sess_destroy();
         redirect('LoginKontroler');
     }
+  
 }
 
 ?>
